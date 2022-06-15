b75eaf18e77cb5a2bae8dfd9f9ae980da6efe5b3
regal-rahul <regalrahul777@gmail.com>
2022-06-15 21:25:46 +0530

    Testing compare method of bcrypt

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const bcrypt  = require('bcrypt');
 
 const hashPassword = async (pw) => {
-    const salt = await bcrypt.genSalt(10);
+    const salt = await bcrypt.genSalt(12);
     const hash =  await bcrypt.hash(pw, salt);
     console.log(salt);
     console.log(hash);
 }
+const login = async(pw, hashedPw) => {
+    const result = await bcrypt.compare(pw, hashedPw);
+    if (result) {
+        console.log('Logged in Successfully!');
+    } else {
+        console.log('Try Again');
+    }
+}
 
-hashPassword('monkey');
 
+// hashPassword('monkey');
+login('monkey', '$2b$12$B6apUukdqaVc/qZCnCSSNuyGJ5qXnCEvkGvxqhVt/QnHzDuUPNqqe');
