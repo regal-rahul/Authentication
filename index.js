eeed51bce087c55b8157fe7e7cb49eeaa326ee03
regal-rahul <regalrahul777@gmail.com>
2022-06-15 21:32:54 +0530

    Short method to hash password

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const bcrypt  = require('bcrypt');
 
+// const hashPassword = async (pw) => {
+//     const salt = await bcrypt.genSalt(12);
+//     const hash =  await bcrypt.hash(pw, salt);
+//     console.log(salt);
+//     console.log(hash);
+// }
+
+
 const hashPassword = async (pw) => {
-    const salt = await bcrypt.genSalt(12);
-    const hash =  await bcrypt.hash(pw, salt);
-    console.log(salt);
+    const hash =  await bcrypt.hash(pw, 12);
     console.log(hash);
 }
+
+
 const login = async(pw, hashedPw) => {
     const result = await bcrypt.compare(pw, hashedPw);
     if (result) {
