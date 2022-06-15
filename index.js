43829012f44c8937cc53d008e3df76267d278f9b
regal-rahul <regalrahul777@gmail.com>
2022-06-15 21:18:29 +0530

    Passes argument in async

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const bcrypt  = require('bcrypt');
 
-const hashPassword = async () => {
+const hashPassword = async (pw) => {
     const salt = await bcrypt.genSalt(10);
     const hash =  await bcrypt.hash(pw, salt);
     console.log(salt);
